90826d9883ed1a05a7f0d062ce7284ef52a073d3
Ale Maza <almazagit1002@gmail.com>
2024-06-12 12:04:14 +0200

    Documenting the API added

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 from typing import Optional, List
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
 from pydantic import BaseModel
 
-app = FastAPI()
+
+
+
+tags_metadata = [
+    {
+        "name": "Get users",
+        "description": "Get all users",
+    },
+    {
+        "name": "Create user",
+        "description": "Create a new user",
+       
+    },
+     {
+        "name": "Get users by ID",
+        "description": "Get user by ID",
+       
+    },
+]
+
+app = FastAPI(
+    title= "Fast API LMS",
+    description="LMS for managing students and courses",
+    version="0,0.1",
+    contact={
+        "name": "Alejandro",
+        "email": "almazagit1002@gmail.com"
+    },
+    license_info={
+        "name": "MIT"
+    },
+    )
 
 
 users = []
@@ -12,16 +43,19 @@ class User(BaseModel):
     is_active : bool
     bio: Optional[str] = None
 
-@app.get("/users", response_model=List[User])
+@app.get("/users", response_model=List[User], tags=["Get users"])
 async def get_user():
     return users
 
-@app.post("/users")
+@app.post("/users", tags=["Create user"])
 async def create_user(user: User):
     users.append(user)
     return "Success"
 
 
-@app.get("/users/{id}")
-async def get_user(id: int):
-    return users[id]
+@app.get("/users/{id}", tags=["Get users by ID"])
+async def get_user(
+    id: int = Path (..., description = "The ID of the user you want to retrieve", gt=0),
+    q : str = Query(None, max_length=5) 
+    ):
+    return {"useru": users[id], "query": q }
